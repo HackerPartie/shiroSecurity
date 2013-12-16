358e60116ffd63b3b82d4cde0866f2f096bfef86
jens <jens@service-b.org>
2013-12-16 16:59:28 +0100

    initial commit

---
+++ src/main/java/ur/user/Authentication.java
@@ -0,0 +1,19 @@
+package ur.user;
+
+import javax.security.auth.Subject;
+import java.util.logging.Logger;
+
+/**
+ * Created by jens on 12/16/13.
+ */
+public class Authentication {
+
+    public static void main (String[] args) {
+        Authentication auth = new Authentication();
+        String login = "admin";
+        char[] pass = { 'a', 'd', 'm', 'i', 'n'};
+
+    }
+
+
+}
---
+++ src/main/java/ur/user/ShiroBoneCPDataSource.java
@@ -0,0 +1,23 @@
+package ur.user;
+
+import com.jolbox.bonecp.BoneCPDataSource;
+
+/**
+ * Created by jens on 12/16/13.
+ */
+public class ShiroBoneCPDataSource extends BoneCPDataSource {
+
+    public ShiroBoneCPDataSource() {
+        super();
+        this.setDriverClass("com.mysql.jdbc.Driver");
+        this.setJdbcUrl("jdbc:mysql://localhost:3306/spring_test");
+        this.setUsername("jens");
+        this.setPassword("option");
+        this.setMaxConnectionsPerPartition(10);
+    }
+
+    public ShiroBoneCPDataSource(BoneCPDataSource config) {
+        super(config);
+    }
+
+}
